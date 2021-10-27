e7f7b4673703e2b9096cf53a7906eddf984bcf5a
sagarkunayak <sagarkunayak104@gmail.com>
2021-10-27 21:05:47 +0530

    first commit

--- demo2/settings.py
+++ demo2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '@d^a%0kmnm+j7gb5h9&_@-fcyi)t^r2+45+b%wnom8@%k*fh99'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
